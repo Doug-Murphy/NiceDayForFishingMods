64ea8f7aed0d1cb9e7599e762104b38313fe70ae
Doug Murphy <doug.murphy82@gmail.com>
2025-09-28 15:57:30 -0400

    Initial commit with stubbed mod.

---
+++ GuysNight.NiceDayForFishingMod.FishStars/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace GuysNight.NiceDayForFishingMod.FishStars;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    public override void Load()
+    {
+        SharedComponents.Logger = Log;
+
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        SharedComponents.Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        SharedComponents.ConfigFile = new ConfigFile(Path.Combine(Paths.ConfigPath, $"{MyPluginInfo.PLUGIN_NAME}.cfg"), true) { SaveOnConfigSet = false };
+        
+        SharedComponents.ConfigFile.Bind(
+            "Feature Toggles",
+            "EnableMod",
+            true,
+            "Whether or not to enable this mod. True for enabling the mod. False for disabling."
+        );
+        
+        SharedComponents.ConfigFile.Save();
+    }
+}
---
+++ GuysNight.NiceDayForFishingMod.FishStars/SharedComponents.cs
@@ -0,0 +1,10 @@
+﻿using BepInEx.Configuration;
+using BepInEx.Logging;
+
+namespace GuysNight.NiceDayForFishingMod.FishStars;
+
+internal static class SharedComponents {
+	internal static ManualLogSource Logger { get; set; }
+
+	internal static ConfigFile ConfigFile { get; set; }
+}
