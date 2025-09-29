135bace3f1443578e06da704d3c2b72c4e2ae193
Doug Murphy <doug.murphy82@gmail.com>
2025-09-28 20:20:35 -0400

    Separate out instant win to separate C# project.

---
+++ GuysNight.NiceDayForFishingMod.InstantWin/Patches/FishEnemyPatches.cs
@@ -0,0 +1,17 @@
+﻿using FusionPlay.Enemy;
+using FusionPlay.GamePlay.YABS.BattleSystem;
+using FusionPlay.Player;
+using HarmonyLib;
+
+namespace GuysNight.NiceDayForFishingMod.InstantWin.Patches;
+
+[HarmonyPatch(typeof(FishEnemy))]
+public class FishEnemyPatches
+{
+    [HarmonyPatch("StartBattle")]
+    [HarmonyPostfix]
+    public static void StartBattlePostfix(FishEnemy __instance, BattleManagerBase battleManager, Player player)
+    {
+        __instance.TakeDamage(5_000_000, false, false);
+    }
+}
