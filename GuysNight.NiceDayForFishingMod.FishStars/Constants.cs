8c1cd3d9db88a14bb334599ad13a5bf838adfe0a
Doug Murphy <doug.murphy82@gmail.com>
2025-09-29 19:06:35 -0400

    Add feature toggle based on config entry for FishStars.

---
+++ GuysNight.NiceDayForFishingMod.FishStars/Constants.cs
@@ -0,0 +1,6 @@
+﻿namespace GuysNight.NiceDayForFishingMod.FishStars;
+
+internal static class Constants {
+	internal const string ConfigSectionHeaderToggles = "Feature Toggles";
+	internal const string ConfigKeyEnableMod = "EnableMod";
+}
--- GuysNight.NiceDayForFishingMod.FishStars/Patches/FishInfoPatches.cs
+++ GuysNight.NiceDayForFishingMod.FishStars/Patches/FishInfoPatches.cs
@@ -9,6 +9,22 @@ public class FishInfoPatches
     [HarmonyPostfix]
     public static void StartPostfix(FishInfo __instance)
     {
+        SharedComponents.ConfigFile.Reload();
+
+        var isModEnabled = true;
+
+        if (SharedComponents.ConfigFile.TryGetEntry<bool>(Constants.ConfigSectionHeaderToggles, Constants.ConfigKeyEnableMod, out var featureToggleConfigEntry)) {
+            isModEnabled = featureToggleConfigEntry.Value;
+            SharedComponents.Logger.LogDebug($"Successfully retrieved mod feature toggle. Value is '{isModEnabled}'");
+        }
+        else {
+            SharedComponents.Logger.LogWarning("Could not retrieve mod feature toggle from config. Assuming it was set to true.");
+        }
+
+        if (!isModEnabled) {
+            return;
+        }
+        
         var targetFishRating = 5; //fishRating only changes the LEVEL of the fish as DISPLAYED while battling it. The rating is actually based on the WEIGHT and calculated after catch.
         var targetFishWeight = __instance.fishWeight * 4; //quadrupling the weight should safely get all fish to 5 stars without being too crazy.
         SharedComponents.Logger.LogInfo($"Setting fish {__instance.name} to have fishRating {targetFishRating} and fishWeight {targetFishWeight}");
