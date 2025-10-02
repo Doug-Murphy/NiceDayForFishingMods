7a63f53b18b9d6c9051d5275602413838b5151c4
Doug Murphy <doug.murphy82@gmail.com>
2025-10-02 18:34:58 -0400

    Run format on FishStars now that I got my config back to normal.

--- GuysNight.NiceDayForFishingMod.FishStars/Plugin.cs
+++ GuysNight.NiceDayForFishingMod.FishStars/Plugin.cs
@@ -8,23 +8,21 @@ using HarmonyLib;
 namespace GuysNight.NiceDayForFishingMod.FishStars;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-public class Plugin : BasePlugin
-{
-    public override void Load()
-    {
-        SharedComponents.Logger = Log;
+public class Plugin : BasePlugin {
+	public override void Load() {
+		SharedComponents.Logger = Log;
 
-        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
-        SharedComponents.Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
-        SharedComponents.ConfigFile = new ConfigFile(Path.Combine(Paths.ConfigPath, $"{MyPluginInfo.PLUGIN_NAME}.cfg"), true) { SaveOnConfigSet = false };
-        
-        SharedComponents.ConfigFile.Bind(
-            Constants.ConfigSectionHeaderToggles,
-            Constants.ConfigKeyEnableMod,
-            true,
-            "Whether or not to enable this mod. True for enabling the mod. False for disabling."
-        );
-        
-        SharedComponents.ConfigFile.Save();
-    }
+		Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+		SharedComponents.Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+		SharedComponents.ConfigFile = new ConfigFile(Path.Combine(Paths.ConfigPath, $"{MyPluginInfo.PLUGIN_NAME}.cfg"), true) { SaveOnConfigSet = false };
+
+		SharedComponents.ConfigFile.Bind(
+			Constants.ConfigSectionHeaderToggles,
+			Constants.ConfigKeyEnableMod,
+			true,
+			"Whether or not to enable this mod. True for enabling the mod. False for disabling."
+		);
+
+		SharedComponents.ConfigFile.Save();
+	}
 }
