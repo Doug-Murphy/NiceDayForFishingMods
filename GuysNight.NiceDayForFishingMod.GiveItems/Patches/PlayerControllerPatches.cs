ca881a5a3553f6ae88a923bd687d4f52f5d4065d
Doug Murphy <doug.murphy82@gmail.com>
2025-10-02 18:35:45 -0400

    Get keybind injection working.

---
+++ GuysNight.NiceDayForFishingMod.GiveItems/Patches/PlayerControllerPatches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using UnityEngine.InputSystem;
+
+namespace GuysNight.NiceDayForFishingMod.GiveItems.Patches;
+
+[HarmonyPatch(typeof(PlayerController))]
+public class PlayerControllerPatches {
+	[HarmonyPatch("Update")]
+	[HarmonyPostfix]
+	public static void Postfix() {
+		var kb = Keyboard.current;
+		if (kb == null) {
+			return; // Not initialized yet
+		}
+
+		if (kb.f1Key.wasPressedThisFrame) {
+			SharedComponents.Logger.LogInfo("F1 pressed: executing feature.");
+		}
+		else {
+			SharedComponents.Logger.LogInfo("F1 not pressed");
+		}
+	}
+}
