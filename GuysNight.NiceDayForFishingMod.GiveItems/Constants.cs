b23e313fb36a83617cf3da432504ae787e4aed8c
Doug Murphy <doug.murphy82@gmail.com>
2025-10-02 20:38:08 -0400

    Catalog all bait and building material item names.

--- GuysNight.NiceDayForFishingMod.GiveItems/Constants.cs
+++ GuysNight.NiceDayForFishingMod.GiveItems/Constants.cs
@@ -6,4 +6,30 @@ internal static class Constants {
 
 	internal const string ConfigSectionHeaderKeybinds = "Keybinds";
 	internal const string ConfigKeyGiveHoneywoodItems = "Keybind for giving all building materials needed to fully level up Honeywood.";
+
+	internal static class ItemNames {
+		internal static class Bait {
+			internal const string ArgonsMushroom = "ItemBait03_Mushrooms";
+			internal const string BoBoBerry = "ItemBait04_Berry";
+			internal const string Fairy = "ItemBait06_Fairy";
+			internal const string Garlic = "ItemBait01_Garlic";
+			internal const string PalmNut = "ItemBait05_PalmNuts";
+			internal const string Worm = "ItemBait02_Worm";
+		}
+
+		internal static class BuildingMaterials {
+			internal const string DriftWood = "ItemBuildingMaterial01";
+			internal const string Ectoplasm = "ItemBuildingMaterial08";
+			internal const string Fossil = "ItemBuildingMaterial07";
+			internal const string IronOre = "ItemBuildingMaterial03";
+			internal const string Obsidian = "ItemBuildingMaterial11";
+			internal const string RawCrystal = "ItemBuildingMaterial10";
+			internal const string RuneStone = "ItemBuildingMaterial09";
+			internal const string SandDollar = "ItemBuildingMaterial05";
+			internal const string Seashell = "ItemBuildingMaterial06";
+			internal const string Slime = "ItemBuildingMaterial04";
+			internal const string SoulGem = "ItemBuildingMaterial12";
+			internal const string Stone = "ItemBuildingMaterial02";
+		}
+	}
 }
