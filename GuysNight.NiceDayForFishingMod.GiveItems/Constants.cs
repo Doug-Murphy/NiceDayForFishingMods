5d4774f7a944d0b73e93b91f439b3bb1c995c204
Doug Murphy <doug.murphy82@gmail.com>
2025-10-03 19:31:18 -0400

    Add more item IDs that I data mined.

--- GuysNight.NiceDayForFishingMod.GiveItems/Constants.cs
+++ GuysNight.NiceDayForFishingMod.GiveItems/Constants.cs
@@ -31,5 +31,126 @@ internal static class Constants {
 			internal const string SoulGem = "ItemBuildingMaterial12";
 			internal const string Stone = "ItemBuildingMaterial02";
 		}
+
+		internal static class Consumables {
+			internal const string HealthPotion = "ItemConsumable01_HealthPotion";
+			internal const string StaminaPotion = "ItemConsumable02_StaminaPotion";
+			internal const string StrengthPotion = "ItemConsumable03_StrenghtPotion"; //intentional typo in internal ID
+			internal const string DefensePotion = "ItemConsumable04_DefensePotion";
+			internal const string EffectRemoveFood = "ItemConsumable05_EffectRemoveFood";
+			internal const string StaminaFood = "ItemConsumable06_StaminaFood";
+			internal const string HealthFood = "ItemConsumable07_HealthFood";
+			internal const string ImmuneFood = "ItemConsumable08_ImmuneFood";
+		}
+		
+		internal static class FishingRods
+		{
+			internal const string TrainingRod = "ItemEquipmentFishingRod01_TrainingRod";
+			internal const string CommonRod = "ItemEquipmentFishingRod02_CommonRod";
+			internal const string ExpertRod = "ItemEquipmentFishingRod03_ExpertRod";
+			internal const string ProfiRod = "ItemEquipmentFishingRod04_ProfiRod";
+			internal const string BladeRod = "ItemEquipmentFishingRod05_BladeRod";
+			internal const string BruteRod = "ItemEquipmentFishingRod06_BruteRod";
+			internal const string WizardRod = "ItemEquipmentFishingRod07_WizardRod";
+			internal const string PickAxeRod = "ItemEquipmentFishingRod08_PickAxeRod";
+			internal const string GoldRod = "ItemEquipmentFishingRod09_GoldRod";
+			internal const string HolyRod = "ItemEquipmentFishingRod10_HolyRod";
+			internal const string KingsRod = "ItemEquipmentFishingRod11_KingsRod";
+			internal const string BaradunsRod = "ItemEquipmentFishingRod12_BaradunsRod";
+			internal const string BodgersHammerRod = "ItemEquipmentFishingRod13_BodgersHammerRod";
+			internal const string GarlicSwordRod = "ItemEquipmentFishingRod14_GarlicSwordRod";
+			internal const string DragonRod = "ItemEquipmentFishingRod15_DragonRod";
+			internal const string ThurdotRod = "ItemEquipmentFishingRod16_ThurdothRod";
+			internal const string StrongRod = "ItemEquipmentFishingRod17_StrongRod";
+			internal const string ShieldRod = "ItemEquipmentFishingRod18_ShieldRod";
+			internal const string AncientRod = "ItemEquipmentFishingRod19_AncientRod";
+			internal const string DLCRod01 = "ItemEquipmentFishingRod20_DLCRod01"; //Not sure if this is "Legendary Rod" or "Overpriced Cosmetic Item" Rod
+			internal const string DLCRod02 = "ItemEquipmentFishingRod21_DLCRod02"; //Not sure if this is "Legendary Rod" or "Overpriced Cosmetic Item" Rod
+		}
+		
+		internal static class Hats
+		{
+			internal const string BaelinHat = "ItemEquipmentHat00_BaelinHat";
+			internal const string Hat01 = "ItemEquipmentHat01";
+			internal const string Hat02 = "ItemEquipmentHat02";
+			internal const string Hat03 = "ItemEquipmentHat03";
+			internal const string Hat04 = "ItemEquipmentHat04";
+			internal const string Hat05 = "ItemEquipmentHat05";
+			internal const string Hat06 = "ItemEquipmentHat06";
+			internal const string Hat07 = "ItemEquipmentHat07";
+			internal const string Hat08 = "ItemEquipmentHat08";
+			internal const string Hat09 = "ItemEquipmentHat09";
+			internal const string Hat10 = "ItemEquipmentHat10";
+			internal const string Hat11 = "ItemEquipmentHat11";
+			internal const string Hat12 = "ItemEquipmentHat12";
+			internal const string Hat13 = "ItemEquipmentHat13";
+			internal const string Hat14 = "ItemEquipmentHat14";
+			internal const string Hat15 = "ItemEquipmentHat15";
+			internal const string DLC1Hat01 = "ItemEquipmentHat16_DLC1Hat01"; //Not sure if this is "Legendary Hat" or "Tinfoil Hat"
+			internal const string DLC1Hat02 = "ItemEquipmentHat17_DLC1Hat02"; //Not sure if this is "Legendary Hat" or "Tinfoil Hat"
+		}
+		
+		internal static class Necklaces
+		{
+			internal const string None = "ItemEquipmentNecklace00_None";
+			internal const string MinorDamageSpellNecklace = "ItemEquipmentNecklace01_MinorDamageSpellNecklace";
+			internal const string WarriorNecklace = "ItemEquipmentNecklace02_WarriorNecklace";
+			internal const string MinorHealNecklace = "ItemEquipmentNecklace03_MinorHealNecklace";
+			internal const string ArmorNecklace = "ItemEquipmentNecklace04_ArmorNecklace";
+			internal const string PowerNecklace = "ItemEquipmentNecklace05_PowerNecklace";
+			internal const string PaladinNecklace = "ItemEquipmentNecklace06_PaladinNecklace";
+			internal const string WizardsNecklace = "ItemEquipmentNecklace07_WizardsNecklace";
+			internal const string HeroNecklace = "ItemEquipmentNecklace08_HeroNecklace";
+			internal const string HighPaladinNecklace = "ItemEquipmentNecklace09_HighPaladinNecklace";
+			internal const string HighWizardNecklace = "ItemEquipmentNecklace10_HighWizardNecklace";
+			internal const string DamageSpellNecklace = "ItemEquipmentNecklace11_DamageSpellNecklace";
+			internal const string BoostNecklace = "ItemEquipmentNecklace12_BoostNecklace";
+			internal const string DotNecklace = "ItemEquipmentNecklace13_DotNecklace";
+			internal const string BigArmorNecklace = "ItemEquipmentNecklace14_BigArmorNecklace";
+			internal const string DLC1Necklace01 = "ItemEquipmentNecklace15_DLC1Necklace01"; //Not sure if this is "Legendary Necklace" or "Company Property Necklace"
+			internal const string DLC1Necklace02 = "ItemEquipmentNecklace16_DLC1Necklace02"; //Not sure if this is "Legendary Necklace" or "Company Property Necklace"
+		}
+		
+		internal static class Rings
+		{
+			internal const string None = "ItemEquipmentJewelry00_None";
+			internal const string StrengthRing = "ItemEquipmentJewelry01_StrengthRing";
+			internal const string StaminaRing = "ItemEquipmentJewelry02_StaminaRing";
+			internal const string StaminaRegRing = "ItemEquipmentJewelry03_StaminaRegRing";
+			internal const string BigStaminaRing = "ItemEquipmentJewelry04_BigStaminaRing";
+			internal const string HealthRegRing = "ItemEquipmentJewelry05_HealthRegRing";
+			internal const string SuperStaminaRing = "ItemEquipmentJewelry06_SuperStaminaRing";
+			internal const string SuperStaminaRegRing = "ItemEquipmentJewelry07_SuperStaminaRegRing";
+			internal const string SuperHealthRegRing = "ItemEquipmentJewelry08_SuperHealthRegRing";
+			internal const string Boss1Ring = "ItemEquipmentJewelry09_Boss1Ring";
+			internal const string Boss2Ring = "ItemEquipmentJewelry10_Boss2Ring";
+			internal const string Boss3Ring = "ItemEquipmentJewelry11_Boss3Ring";
+			internal const string BigStaminaEgRing = "ItemEquipmentJewelry12_BigStaminaEgRing";
+			internal const string SmallHealthRegRing = "ItemEquipmentJewelry13_SmallHealthRegRing";
+			internal const string TraingRing = "ItemEquipmentJewelry14_TraingRing";
+			internal const string DLC1Ring01 = "ItemEquipmentJewelry15_DLC1Ring01"; //Not sure if this is "Legendary Ring" or "Ring of Dormor"
+			internal const string DLC1Ring02 = "ItemEquipmentJewelry16_DLC1Ring02"; //Not sure if this is "Legendary Ring" or "Ring of Dormor"
+		}
+		
+		internal static class Sails
+		{
+			internal const string VLDL = "ItemEquipmentSail01_VLDL";
+			internal const string Stripes = "ItemEquipmentSail02_Stripes";
+			internal const string Wave = "ItemEquipmentSail03_Wave";
+			internal const string Landscape = "ItemEquipmentSail04_Landscape";
+			internal const string Angler = "ItemEquipmentSail05_Angler";
+			internal const string Shell = "ItemEquipmentSail06_Shell";
+			internal const string Pirate = "ItemEquipmentSail07_Pirate";
+			internal const string Lava = "ItemEquipmentSail08_Lava";
+			internal const string Repaired = "ItemEquipmentSail09_Repaired";
+			internal const string Heart = "ItemEquipmentSail10_Heart";
+			internal const string Garlic = "ItemEquipmentSail11_Garlic";
+			internal const string Sail12 = "ItemEquipmentSail12";
+			internal const string Sail13 = "ItemEquipmentSail13";
+			internal const string DLC1Sail01 = "ItemEquipmentSail14_DLC1Sail01"; //I think this is another VLDL one?
+			internal const string DLC1Sail02 = "ItemEquipmentSail15_DLC1Sail02"; //I think this is the worm one
+			internal const string DLC1Sail03 = "ItemEquipmentSail16_DLC1Sail03"; //I think this is Dingo
+			internal const string DLC1Sail04 = "ItemEquipmentSail17_DLC1Sail04"; //I think this is the rainbow mushroom one.
+		}
 	}
 }
