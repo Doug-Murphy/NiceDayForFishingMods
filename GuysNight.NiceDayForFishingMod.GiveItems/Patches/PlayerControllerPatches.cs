7c77c5131bb25830b3aa46f12a610a982e93bbcb
Doug Murphy <doug.murphy82@gmail.com>
2025-10-08 18:18:38 -0400

    Refactor giving bait to be cleaner.

--- GuysNight.NiceDayForFishingMod.GiveItems/Patches/PlayerControllerPatches.cs
+++ GuysNight.NiceDayForFishingMod.GiveItems/Patches/PlayerControllerPatches.cs
@@ -21,22 +21,6 @@ public class PlayerControllerPatches {
 		}
 
 		if (_kb.f1Key.wasPressedThisFrame) {
-			SharedComponents.ConfigFile.Reload();
-
-			var isModEnabled = true;
-
-			if (SharedComponents.ConfigFile.TryGetEntry<bool>(Constants.ConfigSectionHeaderToggles, Constants.ConfigKeyEnableMod, out var featureToggleConfigEntry)) {
-				isModEnabled = featureToggleConfigEntry.Value;
-				SharedComponents.Logger.LogDebug($"Successfully retrieved mod feature toggle. Value is '{isModEnabled}'");
-			}
-			else {
-				SharedComponents.Logger.LogWarning("Could not retrieve mod feature toggle from config. Assuming it was set to true.");
-			}
-
-			if (!isModEnabled) {
-				return;
-			}
-
 			SharedComponents.Logger.LogDebug("F1 pressed: Attempting to give bait.");
 
 			if (_inventoryInfo?.ItemEntries == null) {
@@ -44,116 +28,141 @@ public class PlayerControllerPatches {
 				return;
 			}
 
-			foreach (var entry in _inventoryInfo.ItemEntries) {
-				CacheItem(entry.item);
-			}
+			CacheItemReferences();
 
-			//We can only call AddItem() if we have an ItemBase reference, so we need to look them up from our cached dictionary.
-			//I do not see a way to create new ItemBase instances directly since this is using IL2CPP.
-			if (SharedComponents.BaitByName.TryGetValue(Constants.ItemNames.Bait.ArgonsMushroom, out var argonsMushroom)) {
-				_inventoryInfo.AddItem(argonsMushroom, 100);
-			}
-
-			if (SharedComponents.BaitByName.TryGetValue(Constants.ItemNames.Bait.BoBoBerry, out var boboBerry)) {
-				_inventoryInfo.AddItem(boboBerry, 100);
-			}
-
-			if (SharedComponents.BaitByName.TryGetValue(Constants.ItemNames.Bait.Fairy, out var fairy)) {
-				_inventoryInfo.AddItem(fairy, 100);
-			}
-
-			if (SharedComponents.BaitByName.TryGetValue(Constants.ItemNames.Bait.Garlic, out var garlic)) {
-				_inventoryInfo.AddItem(garlic, 100);
-			}
-
-			if (SharedComponents.BaitByName.TryGetValue(Constants.ItemNames.Bait.PalmNut, out var palmNut)) {
-				_inventoryInfo.AddItem(palmNut, 100);
-			}
-
-			if (SharedComponents.BaitByName.TryGetValue(Constants.ItemNames.Bait.WrigglingWorm, out var wrigglingWorm)) {
-				_inventoryInfo.AddItem(wrigglingWorm, 100);
-			}
+			GivePlayerBait();
 		}
 	}
 
-	private static void CacheItem(ItemBase item) {
-		if (item == null) {
-			return;
-		}
+	/// <summary>
+	/// Give the player some bait items.
+	/// </summary>
+	private static void GivePlayerBait() {
+		SharedComponents.ConfigFile.Reload();
 
-		var key = item.name;
-		if (string.IsNullOrWhiteSpace(key)) {
-			return;
-		}
+		var isModEnabled = true;
 
-		if (item.name == Constants.ItemNames.Gold) {
-			SharedComponents.GoldItem = item;
-			SharedComponents.Logger.LogDebug($"Cached gold: {key}");
-		}
-		else if (ItemUtility.ItemIsBait(item.name)) {
-			if (SharedComponents.BaitByName.TryAdd(key, item)) {
-				SharedComponents.Logger.LogDebug($"Cached bait: {key}");
-			}
-		}
-		else if (ItemUtility.ItemIsBuildingMaterial(item.name)) {
-			if (SharedComponents.BuildingMaterialsByName.TryAdd(key, item)) {
-				SharedComponents.Logger.LogDebug($"Cached building material: {key}");
-			}
-		}
-		else if (ItemUtility.ItemIsConsumable(item.name)) {
-			if (SharedComponents.ConsumablesByName.TryAdd(key, item)) {
-				SharedComponents.Logger.LogDebug($"Cached consumable: {key}");
-			}
-		}
-		else if (ItemUtility.ItemIsFish(item.name)) {
-			if (SharedComponents.FishByName.TryAdd(key, item)) {
-				SharedComponents.Logger.LogDebug($"Cached fish: {key}");
-			}
-		}
-		else if (ItemUtility.ItemIsFishingRod(item.name)) {
-			if (SharedComponents.FishingRodsByName.TryAdd(key, item)) {
-				SharedComponents.Logger.LogDebug($"Cached fishing rod: {key}");
-			}
-		}
-		else if (ItemUtility.ItemIsHat(item.name)) {
-			if (SharedComponents.HatsByName.TryAdd(key, item)) {
-				SharedComponents.Logger.LogDebug($"Cached hat: {key}");
-			}
-		}
-		else if (ItemUtility.ItemIsHook(item.name)) {
-			if (SharedComponents.HooksByName.TryAdd(key, item)) {
-				SharedComponents.Logger.LogDebug($"Cached hook: {key}");
-			}
-		}
-		else if (ItemUtility.ItemIsHookAddon(item.name)) {
-			if (SharedComponents.HookAddonsByName.TryAdd(key, item)) {
-				SharedComponents.Logger.LogDebug($"Cached hook addon: {key}");
-			}
-		}
-		else if (ItemUtility.ItemIsLine(item.name)) {
-			if (SharedComponents.LinesByName.TryAdd(key, item)) {
-				SharedComponents.Logger.LogDebug($"Cached line: {key}");
-			}
-		}
-		else if (ItemUtility.ItemIsNecklace(item.name)) {
-			if (SharedComponents.NecklacesByName.TryAdd(key, item)) {
-				SharedComponents.Logger.LogDebug($"Cached necklace: {key}");
-			}
-		}
-		else if (ItemUtility.ItemIsRing(item.name)) {
-			if (SharedComponents.RingsByName.TryAdd(key, item)) {
-				SharedComponents.Logger.LogDebug($"Cached ring: {key}");
-			}
-		}
-		else if (ItemUtility.ItemIsSail(item.name)) {
-			if (SharedComponents.SailsByName.TryAdd(key, item)) {
-				SharedComponents.Logger.LogDebug($"Cached sail: {key}");
-			}
+		if (SharedComponents.ConfigFile.TryGetEntry<bool>(Constants.ConfigSectionHeaderToggles, Constants.ConfigKeyEnableMod, out var featureToggleConfigEntry)) {
+			isModEnabled = featureToggleConfigEntry.Value;
+			SharedComponents.Logger.LogDebug($"Successfully retrieved mod feature toggle. Value is '{isModEnabled}'");
 		}
 		else {
-			SharedComponents.Logger.LogWarning($"Item is of unknown type. Unable to cache it. item.name is {item.name}");
-			SharedComponents.Logger.LogDebug($"Player has '{_inventoryInfo.GetItemCount(item):N0}' of this item.");
-			SharedComponents.Logger.LogDebug($"item.name is '{item.name}' item.ItemName is '{item.ItemName}' item.ItemNameAlias is '{item.ItemNameAlias}' item.ItemDescription is '{item.ItemDescription}' item.ItemMoneyValue is '{item.ItemMoneyValue}'");
+			SharedComponents.Logger.LogWarning("Could not retrieve mod feature toggle from config. Assuming it was set to true.");
+		}
+
+		if (!isModEnabled) {
+			return;
+		}
+
+		//We can only call AddItem() if we have an ItemBase reference, so we need to look them up from our cached dictionary.
+		//I do not see a way to create new ItemBase instances directly since this is using IL2CPP.
+		if (SharedComponents.BaitByName.TryGetValue(Constants.ItemNames.Bait.ArgonsMushroom, out var argonsMushroom)) {
+			_inventoryInfo.AddItem(argonsMushroom, 100);
+		}
+
+		if (SharedComponents.BaitByName.TryGetValue(Constants.ItemNames.Bait.BoBoBerry, out var boboBerry)) {
+			_inventoryInfo.AddItem(boboBerry, 100);
+		}
+
+		if (SharedComponents.BaitByName.TryGetValue(Constants.ItemNames.Bait.Fairy, out var fairy)) {
+			_inventoryInfo.AddItem(fairy, 100);
+		}
+
+		if (SharedComponents.BaitByName.TryGetValue(Constants.ItemNames.Bait.Garlic, out var garlic)) {
+			_inventoryInfo.AddItem(garlic, 100);
+		}
+
+		if (SharedComponents.BaitByName.TryGetValue(Constants.ItemNames.Bait.PalmNut, out var palmNut)) {
+			_inventoryInfo.AddItem(palmNut, 100);
+		}
+
+		if (SharedComponents.BaitByName.TryGetValue(Constants.ItemNames.Bait.WrigglingWorm, out var wrigglingWorm)) {
+			_inventoryInfo.AddItem(wrigglingWorm, 100);
+		}
+	}
+
+	/// <summary>
+	/// Cache item references in dictionaries so that they can be referenced later when giving items to the player.
+	/// </summary>
+	private static void CacheItemReferences() {
+		foreach (var entry in _inventoryInfo.ItemEntries) {
+			var item = entry.item;
+
+			if (item == null || string.IsNullOrWhiteSpace(item.name)) {
+				continue;
+			}
+
+			var itemName = item.name;
+
+			if (itemName == Constants.ItemNames.Gold) {
+				SharedComponents.GoldItem = item;
+				SharedComponents.Logger.LogDebug($"Cached gold: {itemName}");
+			}
+			else if (ItemUtility.ItemIsBait(itemName)) {
+				if (SharedComponents.BaitByName.TryAdd(itemName, item)) {
+					SharedComponents.Logger.LogDebug($"Cached bait: {itemName}");
+				}
+			}
+			else if (ItemUtility.ItemIsBuildingMaterial(itemName)) {
+				if (SharedComponents.BuildingMaterialsByName.TryAdd(itemName, item)) {
+					SharedComponents.Logger.LogDebug($"Cached building material: {itemName}");
+				}
+			}
+			else if (ItemUtility.ItemIsConsumable(itemName)) {
+				if (SharedComponents.ConsumablesByName.TryAdd(itemName, item)) {
+					SharedComponents.Logger.LogDebug($"Cached consumable: {itemName}");
+				}
+			}
+			else if (ItemUtility.ItemIsFish(itemName)) {
+				if (SharedComponents.FishByName.TryAdd(itemName, item)) {
+					SharedComponents.Logger.LogDebug($"Cached fish: {itemName}");
+				}
+			}
+			else if (ItemUtility.ItemIsFishingRod(itemName)) {
+				if (SharedComponents.FishingRodsByName.TryAdd(itemName, item)) {
+					SharedComponents.Logger.LogDebug($"Cached fishing rod: {itemName}");
+				}
+			}
+			else if (ItemUtility.ItemIsHat(itemName)) {
+				if (SharedComponents.HatsByName.TryAdd(itemName, item)) {
+					SharedComponents.Logger.LogDebug($"Cached hat: {itemName}");
+				}
+			}
+			else if (ItemUtility.ItemIsHook(itemName)) {
+				if (SharedComponents.HooksByName.TryAdd(itemName, item)) {
+					SharedComponents.Logger.LogDebug($"Cached hook: {itemName}");
+				}
+			}
+			else if (ItemUtility.ItemIsHookAddon(itemName)) {
+				if (SharedComponents.HookAddonsByName.TryAdd(itemName, item)) {
+					SharedComponents.Logger.LogDebug($"Cached hook addon: {itemName}");
+				}
+			}
+			else if (ItemUtility.ItemIsLine(itemName)) {
+				if (SharedComponents.LinesByName.TryAdd(itemName, item)) {
+					SharedComponents.Logger.LogDebug($"Cached line: {itemName}");
+				}
+			}
+			else if (ItemUtility.ItemIsNecklace(itemName)) {
+				if (SharedComponents.NecklacesByName.TryAdd(itemName, item)) {
+					SharedComponents.Logger.LogDebug($"Cached necklace: {itemName}");
+				}
+			}
+			else if (ItemUtility.ItemIsRing(itemName)) {
+				if (SharedComponents.RingsByName.TryAdd(itemName, item)) {
+					SharedComponents.Logger.LogDebug($"Cached ring: {itemName}");
+				}
+			}
+			else if (ItemUtility.ItemIsSail(itemName)) {
+				if (SharedComponents.SailsByName.TryAdd(itemName, item)) {
+					SharedComponents.Logger.LogDebug($"Cached sail: {itemName}");
+				}
+			}
+			else {
+				SharedComponents.Logger.LogWarning($"Item is of unknown type. Unable to cache it. item.name is {item.name}");
+				SharedComponents.Logger.LogDebug($"Player has '{_inventoryInfo.GetItemCount(item):N0}' of this item.");
+				SharedComponents.Logger.LogDebug($"item.name is '{item.name}' item.ItemName is '{item.ItemName}' item.ItemNameAlias is '{item.ItemNameAlias}' item.ItemDescription is '{item.ItemDescription}' item.ItemMoneyValue is '{item.ItemMoneyValue}'");
+			}
 		}
 	}
 }
